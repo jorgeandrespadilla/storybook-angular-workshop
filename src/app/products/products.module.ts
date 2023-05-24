b51102184a03be2258191b77b66422de84ea2fdb
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2023-05-24 00:02:57 -0500

    Add project components

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ProductCatalogPageComponent } from './pages/product-catalog-page/product-catalog-page.component';
 import { ProductDetailsPageComponent } from './pages/product-details-page/product-details-page.component';
 import { ProductsRoutingModule } from './products-routing.module';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 
 @NgModule({
   declarations: [
     ProductCatalogPageComponent,
-    ProductDetailsPageComponent
+    ProductDetailsPageComponent,
+    ProductCardComponent,
+    ProductListComponent
   ],
   imports: [
     CommonModule,
